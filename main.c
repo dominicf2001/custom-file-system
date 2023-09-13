bedcfb4f60f0f6350a2c55e6f8570c9403e98a95
Dominic <dominicf2001@gmail.com>
2023-09-12 22:07:32 -0400

    add some skeleton structure. Set up getattr for implementation

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <fuse.h>
 
-int main() {
+static int domFS_getattr(const char* path, struct stat* stbuf, struct fuse_file_info *fi) {
 
+    
+    return 0;
+}
+
+static struct fuse_operations domFS_operations {
+    .getattr = domFS_getattr
+};
+
+int main(int argc, char* argv[]) {
+    
+
+    return fuse_main(argc, argv, &domFS_operations, NULL);
 }
 

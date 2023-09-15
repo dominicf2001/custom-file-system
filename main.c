660279731e9f648d963f131983a8264912ebe9a9
Dominic <dominicf2001@gmail.com>
2023-09-15 19:58:44 -0400

    Get basic stat functionality working

--- main.c
+++ main.c
@@ -1,11 +1,31 @@
 #define FUSE_USE_VERSION 31
+
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <fuse.h>
 
 static int domFS_getattr(const char* path, struct stat* stbuf) {
     struct fuse_context* context = fuse_get_context();
-
-    printf("%d\n", context->uid);
+    memset(stbuf, 0, sizeof(struct stat));
+    
+    stbuf->st_gid = context->gid;
+    stbuf->st_uid = context->uid;
+    
+    if (strcmp(path, "/") == 0){
+        stbuf->st_mode = S_IFDIR | 0755;
+        stbuf->st_nlink = 2;
+    }
+    else if (strcmp(path, "/test") == 0){
+        stbuf->st_mode = S_IFREG | 0644;
+        stbuf->st_nlink = 1;
+        stbuf->st_size = 1212;
+    }
+    else {
+        // Indicates file/directory not found
+        return -ENOENT;
+    }
     
     return 0;
 }

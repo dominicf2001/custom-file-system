56977b2bea7f642d2c5cccfba51e4f0d45453d6f
Dominic <dominicf2001@gmail.com>
2023-09-17 20:06:20 -0400

    add selection sort

--- test.c
+++ test.c
@@ -5,6 +5,7 @@ int main(int arc, char* argv[]){
     struct node* root_node = node_construct();
     node_insert(root_node, 2);
     node_insert(root_node, 3);
+    node_insert(root_node, 4);
     
     node_debug_print(root_node);
 }

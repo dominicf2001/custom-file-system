01f8b35141d79af37aaebe9ce74302c5e1b8b896
Dominic <dominicf2001@gmail.com>
2023-10-15 13:29:17 -0400

    inode refactoring

--- main.c
+++ main.c
@@ -10,25 +10,25 @@
 #include <fuse.h>
 #include "btree/btree.h"
 
-unsigned long i_node_counter;
-struct i_node {
-    unsigned long i_node_num;
+struct inode {
     struct stat* stat;
 };
 
 struct btree *tree;
 
-static int domFS_stat_compare(const void *a, const void *b, void *udata) {
-    const struct i_node* i_node_a = a;
-    const struct i_node* i_node_b = b;
-    
-    return i_node_a->i_node_num - i_node_b->i_node_num;
+#define MAX_INODE_COUNT 10000
+unsigned long inode_counter;
+struct inode* inode_lookup[MAX_INODE_COUNT];
+
+static int domFS_stat_compare(const void *a, const void *b, void *udata) {    
+    return (*(unsigned long*)a - *(unsigned long*)b);
 }
 
 static int domFS_getattr(const char* path, struct stat* stbuf) {
     
     memset(stbuf, 0, sizeof(struct stat));
     
+    const struct i_node* i_node = btree_get();
 
     else {
         // Indicates file/directory not found
@@ -45,6 +45,7 @@ static int domFS_mkdir(const char * path, mode_t) {
     
     new_stat->st_gid = context->gid;
     new_stat->st_uid = context->uid;
+    new_stat->st_size = 4096;
     
     if (strcmp(path, "/") == 0){
         new_stat->st_mode = S_IFDIR | 0755;
@@ -53,10 +54,11 @@ static int domFS_mkdir(const char * path, mode_t) {
     else {
         new_stat->st_mode = S_IFREG | 0644;
         new_stat->st_nlink = 1;
-        new_stat->st_size = 1212;
     }
 
-    btree_set(tree, &(struct i_node){.i_node_num = ++i_node_counter, .stat = new_stat});
+    // does not check if exceeds MAX_INODE_COUNT
+    btree_set(tree, &inode_counter);
+    inode_lookup[inode_counter] = &(struct inode){.stat = new_stat};
 
     return 0;
 }
@@ -67,8 +69,9 @@ static struct fuse_operations domFS_operations = {
 };
 
 int main(int argc, char* argv[]) {
-    tree = btree_new(sizeof(struct i_node), 0, domFS_stat_compare, NULL);
-    i_node_counter = 1;
+    tree = btree_new(sizeof(unsigned long), 0, domFS_stat_compare, NULL);
+    memset(inode_lookup, NULL, sizeof(struct inode*) * MAX_INODE_COUNT);
+    inode_counter = 0;
     
     return fuse_main(argc, argv, &domFS_operations, NULL);
 }
